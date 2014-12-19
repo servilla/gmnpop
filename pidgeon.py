3264a7e0fc44bb53578cea4893a637c1ad2275d1
Mark Servilla <mark.servilla@gmail.com>
2014-12-18 21:12:20 -0700

    Add data directory to .gitignore Add new class file pidgeon.py

---
+++ pidgeon.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: pidgeon.py
+
+:Synopsis:
+
+:Author:
+  servilla
+  
+:Created:
+  12/18/14
+"""
+
+__author__ = "servilla"
+
+class Pid:
+
+    def __init__(self, pid_str):
+        self._pid_str = pid_str
+        _pid_parts = self._pid_str.split("/")
+        _pid_size = len(_pid_parts)
+        self._knb_pid = _pid_parts[_pid_size - 1]
+        self._knb_parts = self._knb_pid.split(".")
+        self._scope = self._knb_parts[0]
+        self._identifier = self._knb_parts[1]
+        self._revision = self._knb_parts[2]
+
+    def get_d1_pid(self):
+        return self._pid_str
+
+    def get_knb_pid(self):
+        return self._knb_pid
+
+    def get_scope(self):
+        return self._scope
+
+    def get_identifier(self):
+        return self._identifier
+
+    def get_revision(self):
+        return self._revision
+
+    def get_key(self):
+        return self._scope + "." + self._identifier
